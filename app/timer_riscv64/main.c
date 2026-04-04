82cfeaa153fe06800ef4862424c0f3c2d7e810cf
Sérgio Johann Filho <sergio.johann@acad.pucrs.br>
2026-04-03 23:12:30 -0300

    update and add new applications

---
+++ app/timer_riscv64/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <interrupt.h>
+#include <bsp.h>
+
+void *timer_handler(void)
+{
+    printf("time: %d us\n", (uint32_t)bsp_read_us());
+    
+    return 0;
+}
+
+int main(void)
+{
+    bsp_timer_init(10);
+    bsp_timer_start();
+    while (1);
+    
+    return 0;
+}
+
